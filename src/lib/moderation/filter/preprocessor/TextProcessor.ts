c5f0f7e324d38bf26bafc0ac3d73fc9a27ec725d
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-03-22 15:01:14 -0700

    feat(text-preprocessor): finish initial impl

--- src/lib/moderation/filter/preprocessor/TextProcessor.ts
+++ src/lib/moderation/filter/preprocessor/TextProcessor.ts
@@ -1,38 +1,29 @@
 import { CharacterIterator } from './CharacterIterator';
+import { PreprocessorOutputWriter } from './PreprocessorOutputWriter';
 
-import { getCode, isWhiteSpace } from '@skyra/char';
+import { getCode, getUnicodeCategory, isAscii, UnicodeCategory } from '@skyra/char';
 
-const PIPE_CODEPOINT = getCode('|');
+const VERTICAL_LINE = getCode('|');
+
+const LOWERCASE_A = getCode('a');
+const LOWERCASE_Z = getCode('z');
+
+const UPPERCASE_A = getCode('A');
+const UPPERCASE_Z = getCode('Z');
 
 export class TextPreprocessor {
-	private readonly maxWordLeetSpeakPercentage: number;
-	private readonly minWordLeetSpeak: number;
-	private readonly maxWordLeetSpeak: number;
 	private readonly leetSpeakDictionary = new Map<number, number>();
+	private readonly multiCharacterConfusables = new Map<number, number[]>();
+	private readonly singleCharacterConfusables = new Map<number, number>();
 
-	private readonly maxCharacterRunLength: number;
-	private readonly maxCharacterRunLengthOverrides = new Map<number, number>();
-	private readonly maxSymbolRunLength: number;
-	private readonly maxWhiteSpaceRunLength: number;
-
-	private readonly symbolCharacters?: Set<number>;
-	private readonly blankCharacters?: Set<number>;
-	private readonly spaceCharacters?: Set<number>;
+	private readonly outputWriter: PreprocessorOutputWriter;
+	private readonly inputIterator = new CharacterIterator();
 
 	public constructor({
 		leetSpeakDictionary,
-		maxWordLeetSpeakPercentage,
-		minWordLeetSpeakCount: minWordLeetSpeak,
-		maxWordLeetSpeakCount: maxWordLeetSpeak,
-
+		confusableCharacterDictionary,
 		maxCharacterRunLength,
 		maxCharacterRunLengthOverrides,
-		maxSymbolRunLength,
-		maxWhiteSpaceRunLength,
-
-		symbolCharacters,
-		blankCharacters,
-		whiteSpaceCharacters: spaceCharacters,
 	}: TextPreprocessorOptions) {
 		for (const [base, aliases] of Object.entries(leetSpeakDictionary)) {
 			const originalCodePoint = getCode(base);
@@ -42,54 +33,118 @@ export class TextPreprocessor {
 			}
 		}
 
-		this.maxWordLeetSpeakPercentage = maxWordLeetSpeakPercentage;
-		this.minWordLeetSpeak = minWordLeetSpeak;
-		this.maxWordLeetSpeak = maxWordLeetSpeak;
+		for (const [base, aliases] of confusableCharacterDictionary) {
+			// If the base character is more than 1 Unicode code unit in length:
+			if ([...base].length > 1) {
+				const baseCodes: number[] = [];
+				for (const char of base) {
+					const charCode = getCode(char);
+					const normalizedCharCode = this.leetSpeakDictionary.get(charCode) ?? charCode;
 
-		this.maxCharacterRunLength = maxCharacterRunLength;
+					baseCodes.push(normalizedCharCode);
+				}
+
+				for (const alias of aliases) {
+					const aliasCode = getCode(alias);
+					this.multiCharacterConfusables.set(aliasCode, baseCodes);
+				}
+
+				continue;
+			}
+
+			const baseCode = getCode(base);
+			const normalizedBaseCode = this.leetSpeakDictionary.get(baseCode) ?? baseCode;
+
+			for (const alias of aliases) {
+				const aliasCode = getCode(alias);
+				this.singleCharacterConfusables.set(aliasCode, normalizedBaseCode);
+			}
+		}
+
+		const resolvedMaxCharacterRunLengthOverrides = new Map<number, number>();
 		if (maxCharacterRunLengthOverrides) {
 			for (const [char, count] of Object.entries(maxCharacterRunLengthOverrides)) {
 				const codePoint = getCode(char);
-				this.maxCharacterRunLengthOverrides.set(codePoint, count);
+				resolvedMaxCharacterRunLengthOverrides.set(codePoint, count);
 			}
 		}
-		this.maxSymbolRunLength = maxSymbolRunLength;
-		this.maxWhiteSpaceRunLength = maxWhiteSpaceRunLength;
 
-		if (symbolCharacters) this.symbolCharacters = new Set([...symbolCharacters].map(getCode));
-		if (blankCharacters) this.blankCharacters = new Set([...blankCharacters].map(getCode));
-		if (spaceCharacters) this.spaceCharacters = new Set([...spaceCharacters].map(getCode));
+		this.outputWriter = new PreprocessorOutputWriter({
+			maxCharacterRunLength,
+			maxCharacterRunLengthOverrides: resolvedMaxCharacterRunLengthOverrides,
+		});
 	}
 
 	public run(input: string) {
-		const normalized = input.normalize('NFKD');
+		this.outputWriter.reset();
+		const normalized = input.trim().normalize('NFKD');
 
 		let i = 0;
 		for (; i < normalized.length; i++) {
 			const char = normalized.charCodeAt(i);
-			// Characters with a code less than or equal to 126 are members of
-			// the ASCII character set, used in a huge majority of
-			// conversations. They require very little processing - we can skip
-			// attempting to match confusable characters and also skip stripping
-			// combining marks. There is only one exception, the pipe character
-			// '|': we consider it to be a confusable character for 'L'.
-			if (char > 126 || char === PIPE_CODEPOINT) break;
+			// We consider the vertical line character '|' to be a confusable
+			// character for 'L'.
+			if (char === VERTICAL_LINE || !isAscii(char)) break;
+			this.processCharFast(char, i);
+		}
+
+		// If `i` is not equal to the length of the string, then the loop above
+		// stopped early due to a character not being ASCII. We now have to take
+		// the slow path.
+		if (i !== normalized.length) {
+			this.inputIterator.setPosition(i);
+			for (const char of this.inputIterator) this.processChar(char, this.inputIterator.position);
+		}
+	}
+
+	private processCharFast(char: number, index: number) {
+		// If the character is an uppercase letter, flip the 5th bit to convert
+		// it to lower-case.
+		if (UPPERCASE_A <= char && char <= UPPERCASE_Z) char ^= 0x20;
+		if (LOWERCASE_A <= char && char <= LOWERCASE_Z) return this.outputWriter.writeAlphaCharacter(char, index);
+
+		// Check for leet-speak characters.
+		const normalizedChar = this.leetSpeakDictionary.get(char);
+		if (normalizedChar) return this.outputWriter.writeAlphaCharacter(normalizedChar, index);
+
+		// All other characters are interpreted as separator characters.
+		this.outputWriter.writeSeparator();
+	}
+
+	private processChar(char: number, index: number) {
+		// If the character is an uppercase letter, flip the 5th bit to convert
+		// it to lower-case.
+		if (UPPERCASE_A <= char && char <= UPPERCASE_Z) char ^= 0x20;
+		if (LOWERCASE_A <= char && char <= LOWERCASE_Z) return this.outputWriter.writeAlphaCharacter(char, index);
+
+		// Check if the character is a confusable character.
+		const singleCharacterConfusable = this.singleCharacterConfusables.get(char);
+		if (singleCharacterConfusable) return this.outputWriter.writeAlphaCharacter(singleCharacterConfusable, index);
+
+		const multiCharacterConfusable = this.multiCharacterConfusables.get(char);
+		if (multiCharacterConfusable) {
+			for (const char of multiCharacterConfusable) this.outputWriter.writeAlphaCharacter(char, index);
+			return;
+		}
+
+		const category = getUnicodeCategory(char);
+		switch (category) {
+			case UnicodeCategory.Control:
+			case UnicodeCategory.Format:
+			case UnicodeCategory.SpacingCombiningMark:
+			case UnicodeCategory.EnclosingMark:
+			case UnicodeCategory.NonSpacingMark:
+				// Simply ignore characters of this type.
+				return;
+			default:
+				this.outputWriter.writeSeparator();
 		}
 	}
 }
 
 export interface TextPreprocessorOptions {
 	leetSpeakDictionary: Record<string, string>;
-	maxWordLeetSpeakPercentage: number;
-	minWordLeetSpeakCount: number;
-	maxWordLeetSpeakCount: number;
-
+	confusableCharacterDictionary: Map<string, string>;
 	maxCharacterRunLength: number;
 	maxCharacterRunLengthOverrides?: Record<string, number>;
-	maxSymbolRunLength: number;
-	maxWhiteSpaceRunLength: number;
-
-	symbolCharacters?: Iterable<string>;
-	blankCharacters?: Iterable<string>;
-	whiteSpaceCharacters?: Iterable<string>;
 }
